0b1ddcfa0c1de67a2deba9ef97b5816eaf95e371
afiatamanna06 <afiatamanna06@gmail.com>
2020-05-05 13:23:43 +0600

    Printing.py updated

--- Printing.py
+++ Printing.py
@@ -11,5 +11,13 @@ name = input("Name : ")
 print("Hi",name)
 #exercise 2
 name = input("Name : ")
-favcolor = input("Favourite color : ")
-print(name,"likes",favcolor)
+fav_color = input("Favourite color : ")
+print(name,"likes",fav_color)
+birth_year = input("Birth year : ")
+print(type(birth_year))
+age = 2020 - int(birth_year)
+print(age)
+#exercise 3
+weight_in_lbs = input("Weight (lbs) : ")
+weight_in_kg = float(weight_in_lbs) * 0.453592
+print(weight_in_kg)
