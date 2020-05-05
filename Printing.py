fea0e7a575abd50ebaa7b4549fe6a409254986a9
afiatamanna06 <afiatamanna06@gmail.com>
2020-05-05 12:54:45 +0600

    Printing.py updated

--- Printing.py
+++ Printing.py
@@ -2,10 +2,14 @@ print("hello Afia")
 print("*"*10)
 price = 20
 print(price)
-#exercise
+#exercise 1
 name = "John Smith"
 age = 20
 new_patient = True
 print(new_patient)
 name = input("Name : ")
 print("Hi",name)
+#exercise 2
+name = input("Name : ")
+favcolor = input("Favourite color : ")
+print(name,"likes",favcolor)
