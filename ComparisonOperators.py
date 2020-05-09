c0328b5b3bda3923951dd74d6fe6ea93528c84ba
afiatamanna06 <afiatamanna06@gmail.com>
2020-05-09 14:49:59 +0600

    ComparisonOperators.py updated

---
+++ ComparisonOperators.py
@@ -0,0 +1,8 @@
+#exercise
+name = input()
+if len(name)<3:
+    print("Name must be atleast 3 characters")
+elif len(name)>50:
+    print("Name can be a maximum of 50 characters")
+else:
+    print("Name looks good")
