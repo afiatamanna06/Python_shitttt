4a314b70bd72f581e423cda8d8943a55d2f5e817
afiatamanna06 <afiatamanna06@gmail.com>
2020-05-09 23:59:56 +0600

    WeightConverter.py updated

--- WeightConverter.py
+++ WeightConverter.py
@@ -1,4 +1,9 @@
 weight = input("Weight: ")
 floatweight = float(weight)
 option = input("(L)bs or (K)g: ")
-
+if option == 'L' or option == 'l':
+    kgweight = floatweight * 0.453592
+    print(f'You are {kgweight} kilos')
+elif option == 'K' or option == 'k':
+    lbsweight = floatweight * 2.20462
+    print(f'You are {lbsweight} lbs')
