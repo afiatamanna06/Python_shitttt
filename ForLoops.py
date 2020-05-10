7f99b45de0e837fe7bcf965a90841676745852eb
afiatamanna06 <afiatamanna06@gmail.com>
2020-05-10 22:11:36 +0600

    ForLoops.py updated

---
+++ ForLoops.py
@@ -0,0 +1,14 @@
+for item in 'Python':
+    print(item)
+
+for item in ['apple', 'mango', 'cherry', 4]:
+    print(item)
+
+for item in range(20):
+    print(item)
+
+for item in range(5, 10):
+    print(item)
+
+for item in range(5, 10, 3):
+    print(item)
