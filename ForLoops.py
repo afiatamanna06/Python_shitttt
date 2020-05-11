fc92cdf40a23c77960c4eaa82e03f01d8a4694d9
afiatamanna06 <afiatamanna06@gmail.com>
2020-05-11 13:53:05 +0600

    ForLoops.py updated

--- ForLoops.py
+++ ForLoops.py
@@ -12,3 +12,10 @@ for item in range(5, 10):
 
 for item in range(5, 10, 3):
     print(item)
+
+#exercise
+prices = [10, 20, 30]
+sum = 0
+for item in prices:
+    sum += item
+print(f'Total: {sum}')
