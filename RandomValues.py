4214fd5ef2ad2ade9e1ba735bf563426a65375a0
afiatamanna06 <afiatamanna06@gmail.com>
2020-05-15 14:44:29 +0600

    RandomValues.py updated

--- RandomValues.py
+++ RandomValues.py
@@ -6,3 +6,13 @@ for item in range(10):
 members = ['Yuki', 'Shigure', 'Ayame', 'Kyo', 'Kagura']
 leader = random.choice(members)
 print(leader)
+#exercise
+class Dice:
+    def roll(self):
+        thistuple = tuple((random.randint(1,6), random.randint(1,6)))
+        return thistuple
+
+
+dice = Dice()
+dice_numbers = dice.roll()
+print(dice_numbers)
