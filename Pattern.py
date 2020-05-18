cdcb8f8efdada72d055064ff3b46d4b6b1b7f1fd
afiatamanna06 <afiatamanna06@gmail.com>
2020-05-18 21:58:05 +0600

    Pattern.py updated

--- Pattern.py
+++ Pattern.py
@@ -1,10 +1,16 @@
 number = int(input())
-
+print("One pattern:")
 for i in range(1, number+1, 1):
     stars = ""
     for j in range(1, i+1, 1):
         stars += '*'
         
     print(stars)
+print("Another pattern:")       
+for i in range(1, number+1, 1):
+    stars = ""
+    for j in range(1, i*2, 1):
+        stars += '*'
         
- 
+    print(stars)
+    
