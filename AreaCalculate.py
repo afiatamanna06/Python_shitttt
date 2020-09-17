eda1e6eddbdabf179c64ec52d074205c501fc38d
afiatamanna06 <afiatamanna06@gmail.com>
2020-09-17 11:35:21 +0600

    StringMethods.py updated

--- AreaCalculate.py
+++ AreaCalculate.py
@@ -13,11 +13,12 @@ class Triangle(Shape):
 class Rectangle(Shape):
     def calculateArea(self):
         area = self.dimension1 * self.dimension2
-        print("Rectangle area:",area)
+        return area
+        
         
              
 ABC = Triangle(5, 6)
 ABC.calculateArea()
 
 ABCD = Rectangle(5, 6)
-ABCD.calculateArea()
+print(ABCD.calculateArea())
